f0b28756abdf29091ec33c866c62f1ad9eedd6a6
Mitul Patel <mitulyaar92@gmail.com>
2024-11-28 14:03:02 -0500

    - remove api implementations

--- src/utils/apiEndpoints.ts
+++
@@ -1,7 +0,0 @@
-export const API_ENDPOINTS = {
-    PRODUCTS: {
-      GET_ALL: '/product/getAllProducts',
-      ORDER: '/product/orderItems',
-      GET_ORDERS: '/product/orders',
-    },
-  } as const;
